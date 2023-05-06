eec2742c4eb0226e04af110a9a20d0aae1bd1eec
Petar Nikolov <100172752+Nikolov87@users.noreply.github.com>
2023-05-06 18:32:31 +0100

    String Reversal-solutions:1-2-3

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,36 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+// function reverse(str) {
+//     let reverse = "";
+//     for (let i = str.length - 1; i >= 0; i--) {
+//         reverse += str[i];
+//     }
+//     return reverse;
+// }
+
+//3.solution
+function reverse(str) {
+    return str.split('').reduce((rev, char) => char + rev, '')
+ }
+
+//2.solution
+// function reverse(str) {
+//    let reversed = '';
+
+//    for (let character of str) {
+//     reversed = character + reversed;
+//    }
+//    return reversed
+// }
+
+//1.solution
+// function reverse(str) {
+//     return str
+//     .split('')
+//     .reverse()
+//     .join('');
+// }
 
 module.exports = reverse;
+// console.log(reverse('hello'));
